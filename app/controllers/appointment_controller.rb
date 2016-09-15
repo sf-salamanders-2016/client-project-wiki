d278143f60d55b79f256f66bb19b5a88640d7321
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-15 15:02:18 -0700

    Pre-commit comment

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -1,5 +1,5 @@
 class AppointmentController < ApplicationController
-  def show
-    @appointment = Appointment.find(params[:id])
-  end
+  # def show
+  #   @appointment = Appointment.find(params[:id])
+  # end
 end
