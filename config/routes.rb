90a56d1a338b1a74c5a43897561b6303a13d4cee
Connor Ray <connor.ray.cb@gmail.com>
2016-09-15 16:45:30 -0700

    Creates mentor/student controller and views for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'appointment/show'
+  root "users#index"
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  resources :students
+  resources :mentors
 end
