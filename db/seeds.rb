ec2f462f71766f93c412070613ba6053ad5e06ff
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-15 13:04:43 -0700

    Add seeds file and appointment controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Student.destroy_all
+Mentor.destroy_all
+Appointment.destroy_all
+StudentReview.destroy_all
+MentorReview.destroy_all
+Topic.destroy_all
+
+john = User.create(first_name: "John", last_name: "Doe", email: "john@john.com", password: "johnjohn")
+jane = User.create(first_name: "Jane", last_name: "Roe", email: "jane@jane.com", password: "janejane")
+student = Student.create(user: john)
+mentor = Mentor.create(user: jane)
+appointment = Appointment.create(student: student, mentor: mentor, start_time: Time.now, duration: 30)
+ruby = Topic.create(name: "Ruby")
+mentor.topics << ruby
+StudentReview.create(appointment: appointment, mentor_rating_of_student: 5, mentor_review_of_student: "Great")
+mentor_review = MentorReview.create(appointment: appointment, student_rating_of_mentor: 4, student_review_of_mentor: "Excellent")
+
+
+
+
+
