328875b2fe5f5c3fd106b8b3e4e9347a5f6eb3e2
Dom Brassey <dombrassey@gmail.com>
2016-09-16 08:14:37 -0700

    Fine tuning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,13 @@ class SessionsController < ApplicationController
     @user = User.find_by :email => params[:email]
 
     if @user.nil?
-      p 'if'
       flash[:error] = "Email does not exist, try again."
       redirect_to new_user_path
     elsif @user.password == params[:password]
-      p 'elsif'
       session[:current_user_id] = @user.id
       redirect_to user_url(@user), :success => "You are logged in. Welcome!"
       flash[:notice] = "You are logged in. Welcome!"
     else
-      p 'else'
       flash[:error] = "Password is incorrect, try again."
       redirect_to new_user_path
     end
