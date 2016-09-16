d8362a1b8c8e9ca6d46afa066a51d3264e4129ba
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-16 13:01:55 -0700

    Complete student review form

--- app/controllers/student_reviews_controller.rb
+++ app/controllers/student_reviews_controller.rb
@@ -8,4 +8,20 @@ class StudentReviewsController < ApplicationController
     @appointment = Appointment.find(params[:appointment_id])
     @student_review = StudentReview.find(params[:id])
   end
+
+  def create
+    @appointment = Appointment.find(params[:appointment_id])
+    @student_review = StudentReview.new(student_review_params)
+    @student_review.appointment = @appointment
+    if @student_review.save
+      redirect_to @appointment
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def student_review_params
+    params.require(:student_review).permit(:mentor_rating_of_student, :mentor_review_of_student)
+  end
 end
